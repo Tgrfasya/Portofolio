e808caf642f230f104648da897fd7376c3b2f827
Tgrfasya <mtegaraja2003@gmail.com>
2026-09-15 12:23:36 +0700

    Debug Laravel drivers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('portfolio');
+Route::get('/debug-config', function () {
+    return response()->json([
+        'mail' => config('mail.default'),
+        'cache' => config('cache.default'),
+        'session' => config('session.driver'),
+        'queue' => config('queue.default'),
+        'broadcast' => config('broadcasting.default'),
+        'log' => config('logging.default'),
+    ]);
 });
